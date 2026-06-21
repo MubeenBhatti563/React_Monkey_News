3502791e51ba4682186c3b7cc0b4c0e33cfa6548
Muhammad Mubeen <mubeeniqbal563@gmail.com>
2026-06-21 09:05:04 +0500

    Remove console.log

--- api/news.js
+++ api/news.js
@@ -10,7 +10,6 @@ export default async function handler(req, res) {
   } = req.query;
 
   const apiKey = process.env.React_App_NewsMonkey;
-  console.log("Api Key", apiKey);
 
   if (!apiKey) {
     return res.status(500).json({
